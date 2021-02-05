3faf8d5e5ae78d6bc90ff5853030b0bcab06735f
MyLastSerenade <ssakanek86@googlemail.com>
2021-02-05 22:49:36 +0100

    trying axios for api call

--- commands/btc.js
+++ commands/btc.js
@@ -5,9 +5,10 @@ module.exports = {
     cooldown: 500,
     description: 'Make an api call',
     execute(message, args) {
-        axios.get('https://blockchain.info/tobtc?currency=USD&value=500')
+        axios.get('https://blockchain.info/tobtc?currency=EUR&value=1000')
         .then((res) => {
             console.log('RES', res.data)
+            message.channel.send('1000€ sind zur Zeit ', res.data, ' wert!')
         })
         .catch((err) => {
             console.log('ERR', err)
