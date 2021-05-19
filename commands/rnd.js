cb62fb6c6b692f27893edbfce8c5893812c5a8e6
MyLastSerenade <ssakanek86@googlemail.com>
2021-05-19 13:18:55 +0200

    changed scrapChannel to not return non existing screenshots

--- commands/rnd.js
+++ commands/rnd.js
@@ -17,7 +17,7 @@ module.exports = {
         abcmin = 0;
         abcmax = 23;
         const abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-        
+
         function random() {
             url = "";
             for (i = 0; i < 7; i++) {
@@ -31,23 +31,25 @@ module.exports = {
                     url = url.concat(x);
                 }
             }
-            return url;
+            return 'https://prnt.sc/' + url;
         }
         async function scrapeChannel(url) {
             const browser = await puppeteer.launch(chromeOptions);
             const page = await browser.newPage();
             await page.goto(url);
-        
+
             const [el] = await page.$x('//*[@id="screenshot-image"]');
             const src = await el.getProperty('src');
             const name = await src.jsonValue();
-        
+
             browser.close();
+            if (name == 'https://st.prntscr.com/2021/04/08/1538/img/0_173a7b_211be8ff.png') {
+                scrapeChannel(random());
+            }
             message.channel.send("Random Screenshot " + name);
             return { name };
-        
+
         }
-        random();
-        scrapeChannel('https://prnt.sc/' + url);
+        scrapeChannel(random());
     },
 };
