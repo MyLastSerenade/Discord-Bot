0b35d4dbcb39e5aeb67fbc72bd82e8fc1c39a0ba
MyLastSerenade <ssakanek86@googlemail.com>
2021-08-30 21:06:53 +0200

    bugfix

--- commands/hs.js
+++ commands/hs.js
@@ -16,6 +16,7 @@ module.exports = {
         var xp = '/html/body/div[2]/div[2]/div[4]/div/div/p[1]/b';
         var xp2 = '/html/body/div[2]/div[4]/div/div[1]/div/div/p[1]/b';
         var url = 'https://www.corona-in-zahlen.de/landkreise/lk%20heinsberg/';
+        
         var cn = "im Kreis Heinsberg"
 
         async function asyncscrapeChannelForSevenDayIncidence(url, xpath, cityName) {
@@ -43,8 +44,8 @@ module.exports = {
             message.channel.send("Quelle: " + url);
         }
 
-        asyncscrapeChannelForSevenDayIncidence(xp, url, cn);
-        scrapeChannelForInvasivePathents(xp2, url);
+        asyncscrapeChannelForSevenDayIncidence(url, xp, cn);
+        scrapeChannelForInvasivePathents(url, xp2);
 
     }
 }
