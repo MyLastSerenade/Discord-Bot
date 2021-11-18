63a46c23d03a93704d97a81ed458e44bcb3e3863
MyLastSerenade <ssakanek86@googlemail.com>
2021-11-18 08:20:29 +0100

    changed some xpath to get the correct data again

--- commands/la.js
+++ commands/la.js
@@ -12,22 +12,41 @@ module.exports = {
             args: [
                 "--no-sandbox"
             ],
-        };
-        async function scrapeChannel(url, stadtName) {
+        }
+        var xp = '/html/body/div[2]/div[2]/div[4]/div/div/p[1]/b';
+        var xp2 = '/html/body/div[2]/div[3]/div[2]/div/div/p[1]/b';
+        var url = 'https://www.corona-in-zahlen.de/landkreise/lk%20landshut/';
+
+        var cn = "im Kreis Heinsberg"
+
+        async function asyncscrapeChannelForSevenDayIncidence(url, xpath, cityName) {
             const browser = await puppeteer.launch(chromeOptions);
             const page = await browser.newPage();
             await page.goto(url);
 
-            const [el] = await page.$x('/html/body/div[2]/div[2]/div[4]/div/div/p[1]/b')
+            const [el] = await page.$x(xpath)
             const text = await el.getProperty('textContent');
             const name = await text.jsonValue();
             browser.close();
-            message.channel.send("Die 7-Tage-Inzidenz " + stadtName + " liegt bei: " + name + " Neuinfektionen");
-            message.channel.send("Quelle: " + url);
-            return { name }
+            console.log("Die 7-Tage-Inzidenz " + cityName + " liegt bei: " + name);
         }
-        scrapeChannel('https://www.corona-in-zahlen.de/landkreise/lk%20landshut/', "im Landkreis Landshut");
+
+        async function scrapeChannelForInvasivePathents(url, xpath) {
+            const browser = await puppeteer.launch(chromeOptions);
+            const page = await browser.newPage();
+            await page.goto(url);
+
+            const [el] = await page.$x(xpath)
+            const text = await el.getProperty('textContent');
+            const name = await text.jsonValue();
+            browser.close();
+            console.log("Zur Zeit gibt es " + name + " Intensivmedizinisch behandelte COVID‑19 Patienten!");
+            console.log("Quelle: " + url);
+        }
+
         
+        asyncscrapeChannelForSevenDayIncidence(url, xp, cn);
+        scrapeChannelForInvasivePathents(url, xp2);
 
 
     },
