37f820f95e480ee0fd18cfad7c88dd604c283221
MyLastSerenade <ssakanek86@googlemail.com>
2022-06-20 12:38:32 +0200

    added a null/undefined check

--- commands/rank.js
+++ commands/rank.js
@@ -6,7 +6,7 @@ module.exports = {
     description: 'Get your Rank on the Ladder',
     execute(message, args) {
         var url = "https://api.ageofempires.com/api/ageiv/EventLeaderboard";
-   
+
         var payload = {
             "region": 0,
             "versus": "players",
@@ -15,28 +15,33 @@ module.exports = {
             "searchPlayer": "",
             "page": 1,
             "count": 100
-          }
-          
-        
-        async function aoeivApiRequest(url, name) {
-            payload.searchPlayer = name;
-            await axios.post(url, payload)
-                .then((res) => {
-                    json = res.data
-                    message.channel.send(json.items.length + " Player with the name " + name + " were found!")
-                    json.items.forEach(element => {
-                        message.channel.send("Region: " + element.region)
-                        message.channel.send("ELO: " + element.elo)
-                        message.channel.send("Rank: " + element.rankLevel + " Number " + element.rank + " on the Ladder")
-                    });
-                })
-                .catch((err) => {
-                    console.log('ERR', err)
-                });
         }
-        
+
+
+        async function aoeivApiRequest(url, name) {
+
+            if (name === null || name === undefined || name === "") {
+                message.channel.send("Bitte gib nach dem $Rank Befehl noch einen Namen an!")
+            } else {
+                payload.searchPlayer = name;
+                await axios.post(url, payload)
+                    .then((res) => {
+                        json = res.data
+                        message.channel.send(json.items.length + " Player with the name " + name + " were found!")
+                        json.items.forEach(element => {
+                            message.channel.send("Region: " + element.region)
+                            message.channel.send("ELO: " + element.elo)
+                            message.channel.send("Rank: " + element.rankLevel + " Number " + element.rank + " on the Ladder")
+                        });
+                    })
+                    .catch((err) => {
+                        console.log('ERR', err)
+                        message.channel.send("Es ist etwas schief gelaufen. Bitte gib nach dem $Rank Befehl noch einen Namen an!")
+                    });
+            }
+        }
         aoeivApiRequest(url, String(args));
-   
-   
+
+
     }
 }
